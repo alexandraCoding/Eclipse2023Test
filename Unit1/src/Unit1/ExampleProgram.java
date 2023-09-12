323cd843110688b3ba2d15c39f4d94c65bec2ed1
alexandraCoding <51174166+alexandraCoding@users.noreply.github.com>
2023-09-12 11:39:46 +0100

    created java project tes and A Unit1 folder

---
+++ Unit1/src/Unit1/ExampleProgram.java
@@ -0,0 +1,10 @@
+
+package Unit1;
+public class ExampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("Hello Everyone and world -test");
+	}
+
+}
